2dd23dfaa71a764933da388510e1e0edb505e02c
marcosfcarvalho <andrefrotacarvalho@gmail.com>
2026-09-13 18:00:54 -0300

    Ainda testando

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
 print('hello world')
-
-print('Vai identificar isso aqui agora git?')
-
-# fluxo que preciso decorar para o git/github
-# git status
-# git add .
-# git commit -m "O que eu fiz"
-# git push
